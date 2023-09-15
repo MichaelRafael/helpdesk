bf9bb14fe426094c81101cd307e35305749a7db9
MichaelRafael <michael-ras@hotmail.com>
2023-09-15 14:55:36 -0300

    Criação de mais instâncias para persistir na base de dados

--- src/main/java/com/projeto/helpdesk/services/DBService.java
+++ src/main/java/com/projeto/helpdesk/services/DBService.java
@@ -30,15 +30,26 @@ public class DBService {
 
         Tecnico tec1 = new Tecnico(null, "Michael Rafael", "04131638495", "michael-ras@hotmail.com", "123");
         tec1.addPerfil(Perfil.ADMIN);
+        Tecnico tec2 = new Tecnico(null, "Roberto Alves", "84647581039", "roberto_alves@hotmail.com", "123");
+        Tecnico tec3 = new Tecnico(null, "Camila Pitanga", "66907000032", "camila-pitanga@hotmail.com", "123");
+        tec1.addPerfil(Perfil.ADMIN);
+        Tecnico tec4 = new Tecnico(null, "Magno Malta", "81406873080", "magno_malta@hotmail.com", "123");
 
         Cliente cli1 = new Cliente(null, "Rosangela Conceição", "04448004414", "rosangela@hotmail.com", "123");
+        Cliente cli2 = new Cliente(null, "José da Manga", "41406756083", "jose_manga@hotmail.com", "123");
+        Cliente cli3 = new Cliente(null, "Rodrigo Costa", "71831608073", "rodrigo_costa@hotmail.com", "123");
+        Cliente cli4 = new Cliente(null, "Amanda Costa", "37941111098", "amanda_costa@hotmail.com", "123");
 
-        Chamado chamado1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamdo", tec1, cli1);
+        Chamado chamado1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
+        Chamado chamado2 = new Chamado(null, Prioridade.BAIXA, Status.ABERTO, "Chamado 02", "Segundo chamado", tec1, cli2);
+        Chamado chamado3 = new Chamado(null, Prioridade.BAIXA, Status.ANDAMENTO, "Chamado 03", "Terceiro chamado", tec3, cli3);
+        Chamado chamado4 = new Chamado(null, Prioridade.MEDIA, Status.ABERTO, "Chamado 04", "Quarto chamado", tec2, cli4);
+        Chamado chamado5 = new Chamado(null, Prioridade.ALTA, Status.ANDAMENTO, "Chamado 05", "Quinto chamado", tec4, cli4);
 
-        tecnicoRepository.saveAll(List.of(tec1));
+        tecnicoRepository.saveAll(List.of(tec1, tec2, tec3, tec4));
 
-        clienteRepository.saveAll(List.of(cli1));
+        clienteRepository.saveAll(List.of(cli1, cli2, cli3, cli4));
 
-        chamadoRepository.saveAll(List.of(chamado1));
+        chamadoRepository.saveAll(List.of(chamado1, chamado2, chamado3, chamado4, chamado5));
     }
 }
