ad88970b591d9ce48cbca539ea5415e543832079
MichaelRafael <michael-ras@hotmail.com>
2023-09-15 17:44:07 -0300

    Criação da classe TecnicoService

---
+++ src/main/java/com/projeto/helpdesk/services/TecnicoService.java
@@ -0,0 +1,21 @@
+package com.projeto.helpdesk.services;
+
+import com.projeto.helpdesk.exceptions.ObjectNotFoundException;
+import com.projeto.helpdesk.models.Tecnico;
+import com.projeto.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+    public Tecnico findById(Integer id) {
+        Optional<Tecnico> tecnico = tecnicoRepository.findById(id);
+        return tecnico.orElseThrow(() -> new ObjectNotFoundException("Técnico não encontrado"));
+    }
+}
