f1de2e79ed05ee7f352e6c41c1ee594adf022c2e
MichaelRafael <michael-ras@hotmail.com>
2023-09-15 18:01:56 -0300

    Criação da classe TecnicoController

--- src/main/java/com/projeto/helpdesk/controllers/TecnicoController.java
+++ src/main/java/com/projeto/helpdesk/controllers/TecnicoController.java
@@ -1,2 +1,26 @@
-package com.projeto.helpdesk.controllers;public class TecnicoController {
+package com.projeto.helpdesk.controllers;
+
+import com.projeto.helpdesk.dtos.TecnicoDTO;
+import com.projeto.helpdesk.models.Tecnico;
+import com.projeto.helpdesk.services.TecnicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tecnico")
+public class TecnicoController {
+
+    @Autowired
+    public TecnicoService tecnicoService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
+        Tecnico tecnico = tecnicoService.findById(id);
+        return ResponseEntity.ok().body(new TecnicoDTO(tecnico));
+    }
 }
+
