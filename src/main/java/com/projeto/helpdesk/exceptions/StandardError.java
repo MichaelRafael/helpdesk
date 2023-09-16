6af07db1fb3314ebdaa1ca931798de367f6e459d
MichaelRafael <michael-ras@hotmail.com>
2023-09-16 11:57:20 -0300

    Criação da classe StandardError

--- src/main/java/com/projeto/helpdesk/exceptions/StandardError.java
+++ src/main/java/com/projeto/helpdesk/exceptions/StandardError.java
@@ -1,2 +1,58 @@
-package com.projeto.helpdesk.exceptions;public class StandardError {
+package com.projeto.helpdesk.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public class StandardError {
+
+    @JsonFormat(pattern = "dd/MM/yyy HH:mm")
+    private LocalDateTime timestemp;
+    private Integer status;
+    private String message;
+    private String path;
+
+    public StandardError() {
+        super();
+    }
+
+    public StandardError(LocalDateTime timestemp, Integer status, String message, String path) {
+        super();
+        this.timestemp = timestemp;
+        this.status = status;
+        this.message = message;
+        this.path = path;
+    }
+
+    public LocalDateTime getTimestemp() {
+        return timestemp;
+    }
+
+    public void setTimestemp(LocalDateTime timestemp) {
+        this.timestemp = timestemp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
