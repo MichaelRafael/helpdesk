10140677ade89a75c040aa2cbe25a38dc8479b3e
MichaelRafael <michael-ras@hotmail.com>
2023-09-20 17:41:48 -0300

    Criação da funcionalidade findById para chamado

--- src/main/java/com/projeto/helpdesk/controllers/ChamadoController.java
+++ src/main/java/com/projeto/helpdesk/controllers/ChamadoController.java
@@ -1,2 +1,25 @@
-package com.projeto.helpdesk.controllers;public class ChamadoController {
+package com.projeto.helpdesk.controllers;
+
+import com.projeto.helpdesk.dtos.ChamadoDTO;
+import com.projeto.helpdesk.models.Chamado;
+import com.projeto.helpdesk.services.ChamadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chamado")
+public class ChamadoController {
+
+    @Autowired
+    private ChamadoService chamadoService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
+        Chamado chamado = chamadoService.findById(id);
+        return ResponseEntity.ok().body(new ChamadoDTO(chamado));
+    }
 }
