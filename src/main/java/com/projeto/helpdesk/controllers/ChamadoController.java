5baa472a244d544a77f56208a062ba329ee0009c
MichaelRafael <michael-ras@hotmail.com>
2023-09-20 17:58:12 -0300

    Criação da funcionalidade findAll para chamado

--- src/main/java/com/projeto/helpdesk/controllers/ChamadoController.java
+++ src/main/java/com/projeto/helpdesk/controllers/ChamadoController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/chamado")
 public class ChamadoController {
@@ -21,5 +24,12 @@ public class ChamadoController {
     public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
         Chamado chamado = chamadoService.findById(id);
         return ResponseEntity.ok().body(new ChamadoDTO(chamado));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ChamadoDTO>> findAll() {
+        List<Chamado> list = chamadoService.findAll();
+        List<ChamadoDTO> listDTO = list.stream().map(x -> new ChamadoDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
--- src/main/java/com/projeto/helpdesk/services/ChamadoService.java
+++ src/main/java/com/projeto/helpdesk/services/ChamadoService.java
@@ -6,6 +6,7 @@ import com.projeto.helpdesk.repositories.ChamadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class ChamadoService {
     public Chamado findById(Integer id) {
         Optional<Chamado> chamado = chamadoRepository.findById(id);
         return chamado.orElseThrow(() -> new ObjectNotFoundException("Chamado não encontrado!"));
+    }
+
+    public List<Chamado> findAll() {
+        return chamadoRepository.findAll();
     }
 }
