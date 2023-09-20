0bda9eb064c18838018a35c7e805a4a4cec9dfef
MichaelRafael <michael-ras@hotmail.com>
2023-09-20 17:41:48 -0300

    Crfuncionalidade findById para chamado

--- src/main/java/com/projeto/helpdesk/services/ChamadoService.java
+++ src/main/java/com/projeto/helpdesk/services/ChamadoService.java
@@ -1,2 +1,21 @@
-package com.projeto.helpdesk.services;public class ChamadoService {
+package com.projeto.helpdesk.services;
+
+import com.projeto.helpdesk.exceptions.ObjectNotFoundException;
+import com.projeto.helpdesk.models.Chamado;
+import com.projeto.helpdesk.repositories.ChamadoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository chamadoRepository;
+
+    public Chamado findById(Integer id) {
+        Optional<Chamado> chamado = chamadoRepository.findById(id);
+        return chamado.orElseThrow(() -> new ObjectNotFoundException("Chamado não encontrado!"));
+    }
 }
